b2b2adeaefe2d4d250b88513b109548587a1faf0
Jesus M Arellano III <74021219+Jarellano562@users.noreply.github.com>
2021-04-21 23:28:12 -0700

    new key

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/BudgetTracker";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -27,4 +27,3 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
     console.log(`🌍 Connected on localhost: ${PORT}`);
 });
-
